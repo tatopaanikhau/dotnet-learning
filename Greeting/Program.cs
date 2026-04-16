709c78c6edbf1e411bbbe06acc94a99c2ed75d2e
Nikesh Sherpa <nikeshsherpa.079@kathford.edu.np>
2026-04-17 04:40:35 +0545

    Mini Project 1

---
+++ Greeting/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name: ");
+String name = Console.ReadLine();
+Console.WriteLine("Enter a number: ");
+String times = Console.ReadLine();
+
+for(int i = 0;i< Convert.ToInt32(times); i++)
+{
+    Console.WriteLine(name);
+}
